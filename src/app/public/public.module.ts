7e312055bc74ac588fe90ec95dc3c65e7dccb378
KevinMB22 <73042033+KevinMB22@users.noreply.github.com>
2022-10-10 16:39:47 +0200

    Création de l'architecture des vues

---
+++ src/app/public/public.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    UserComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PublicModule { }
